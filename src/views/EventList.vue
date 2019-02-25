07ab431072b3d39958a5c7fd6f4aa2bace8a158d
linh4 <linh44h@gmail.com>
2019-02-25 14:57:50 -0500

    set up vue with event lists

---
+++ src/views/EventList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Event Listing</h1>
+    <EventCard v-for="(event, idx) in events" :key="idx" :event="event"/>
+  </div>
+</template>
+
+<script>
+  import EventCard from '@/components/EventCard.vue';
+  import EventService from '@/services/EventService.js'
+
+  export default {
+    components: {
+      EventCard
+    },
+    data() {
+      return {
+        events: []
+      }
+    },
+    created() {
+      EventService.getEvents()
+      .then(data => this.events = data.data)
+      .catch(error => console.log(error.response))
+    }
+  }
+</script>
+
+<style lang="css" scoped>
+</style>
